5db9cdf0cbc8b92d8754f5015539c1ab9e142fd2
CipherPixy032 <mollym3gtr@gmail.com>
2023-11-02 16:15:19 +0700

    Upload Tugas experiment 2 No 1

--- ArrayNilai15.java
+++ ArrayNilai15.java
@@ -5,13 +5,10 @@ public class ArrayNilai15 {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-
-        System.out.println("Isi elemen dari array nilaiAkhir:");
-
         for (int i = 0; i < 10; i++) {
             System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
         }
